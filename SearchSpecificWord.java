85828ee907fe94576ebb646b808901f638a548c6
Ujjwal-625 <ujjwalrawat248@gmail.com>
2025-03-25 19:26:44 +0530

    [Author:Ujjwal] ADD:SearchSpecificWord.java

---
+++ SearchSpecificWord.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SearchSpecificWord {
+
+    static void findWord(String s,String word){
+        String [] arr=s.split(" ");
+
+        for(String i:arr){
+            if(arr.equals(word)){
+                System.out.println("Word found");
+                return ;
+            }
+        }
+        System.out.println("Word Not found ");
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter any sentence ");
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.nextLine();
+
+        System.out.println("Enter the word you want to find caseSensitive ");
+
+        String word=sc.next();
+
+        findWord(s,word);
+    }
+}
